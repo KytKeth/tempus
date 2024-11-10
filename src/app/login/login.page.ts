c8b2dffac9e5620d906a910a41ef4658e3262330
KytKeth <ketlenhayram@gmail.com>
2024-11-09 22:20:17 -0300

    sla

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,20 +8,25 @@ import { Storage } from '@ionic/storage-angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  username!: string;
-  password!: string;
+  username: string = '';
+  password: string = '';
 
   constructor(private navCtrl: NavController, private storage: Storage) {}
 
+  async ngOnInit() {
+    await this.storage.create(); 
+  }
+  
   async login() {
     const storedUser = await this.storage.get(this.username);
+    console.log('storedUser:', storedUser);
 
     if (storedUser && storedUser.password === this.password) {
       console.log('Login bem-sucedido');
       this.navCtrl.navigateForward('/inicio');
     } else {
+      console.log('Credenciais inválidas');
       alert('Credenciais inválidas');
     }
   }
 }
-
