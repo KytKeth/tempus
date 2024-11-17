c209f4215de84a0ad20f06d7aaea4bb996aba1db
KytKeth <ketlenhayram@gmail.com>
2024-11-16 23:08:00 -0300

    CARALHO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   //production: false,
-  // production: false,
+  production: false,
   firebaseConfig: {
     apiKey: "AIzaSyBsT8-iZkqKjldNzTFoHYc-xqcisd6htUo",
     authDomain: "tempus-v2.firebaseapp.com",
